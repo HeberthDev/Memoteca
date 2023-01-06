69b508aa034f30150a0bbb10c3371efb6b6ae1a3
Heberth <heberth@onerpm.com>
2023-01-05 22:39:06 -0300

    Navegação dinâmica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamento.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-import { CabecalhoComponent } from './cabecalho/cabecalho.component';
-import { CriarPensamentoComponent } from './pensamentos/criar-pensamentos/criar-pensamento.component';
-import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [			
     AppComponent,
-      CabecalhoComponent,
-      RodapeComponent,
-      CriarPensamentoComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarPensamentoComponent,
+    ListarPensamentoComponent,
    ],
   imports: [
     BrowserModule,

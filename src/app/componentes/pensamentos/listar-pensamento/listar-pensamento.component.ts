3395f800001b80e9fc8b731675eeac403feeb2fe
Heberth Alexandre <heberthdeveloper@gmail.com>
2023-01-08 16:44:01 -0300

    NgClass

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -17,6 +17,11 @@ export class ListarPensamentoComponent implements OnInit {
       autoria: 'Componente filho',
       modelo: 'modelo2',
     },
+    {
+      conteudo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam nulla et neque iste laudantium quos fugiat, beatae asperiores ipsum nihil quidem soluta itaque numquam perferendis at id, error eveniet suscipit. ARas asdasd ad asd asdasdasd as jknqdjw qd  qwd d q  qwd qwd wq',
+      autoria: '',
+      modelo: 'modelo1',
+    },
   ];
 
   constructor() { }

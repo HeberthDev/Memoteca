599e7a136d2da90486a97bbaae5836e31e55dacc
Heberth <heberth@onerpm.com>
2023-01-16 17:43:58 -0300

    Interfaces

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,23 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.scss']
 })
 export class ListarPensamentoComponent implements OnInit {
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo3',
-    },
-    {
-      conteudo: 'Minha propriedadade é decorada com @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo2',
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam nulla et neque iste laudantium quos fugiat, beatae asperiores ipsum nihil quidem soluta itaque numquam perferendis at id, error eveniet suscipit. ARas asdasd ad asd asdasdasd as jknqdjw qd  qwd d q  qwd qwd wq',
-      autoria: '',
-      modelo: 'modelo1',
-    },
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 

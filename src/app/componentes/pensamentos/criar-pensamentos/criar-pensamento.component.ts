d8b34a8de20acc203471bf2875a709e203796ad1
Heberth Alexandre <121596722+juliaandreds@users.noreply.github.com>
2023-01-25 00:02:07 -0300

    Validators pattern e minLength

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamento.component.ts
@@ -20,13 +20,24 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit() {
     this.formulario = this.formBuilder.group({
-      conteudo: ['',[Validators.required]],
-      autoria: ['', [Validators.required]],
+      conteudo: ['', Validators.compose(
+        [
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        ])
+      ],
+      autoria: ['', Validators.compose(
+        [
+          Validators.required,
+          Validators.minLength(3),
+        ])
+      ],
       modelo: ['modelo1'],
     })
   }
 
   criarPensamento() {
+    console.log(this.formulario.status);
     if(this.formulario.valid) {
       this.service.criar(this.formulario.value).subscribe(() =>
         this.router.navigate(['/listarPensamento']),
